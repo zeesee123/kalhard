3c83850e0c12b0bc25f7365bb747917f51a9c88c
zeesee123 <zubinchadha@gmail.com>
2025-05-25 23:09:03 +0530

    template initiation

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express=require('express');
 const app=express();
 
+require('dotenv').config();
 
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+
+    console.log('my project works absolutely fine');
+    res.send('hey');
+});
 
 // hello
+app.listen(process.env.PORT,()=>{
+    console.log('the server is listening');
+});
+
 
