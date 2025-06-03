1f2a0a4fe6256e97e3deea2c42e1594489954c8e
zeesee123 <zubinchadha@gmail.com>
2025-06-03 19:53:54 +0530

    pushing in the changes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 
 require('dotenv').config();
 
 app.set('view engine','ejs');
+
+app.use('/admin/assets',express.static(path.join(__dirname,'public')));
 
 app.get('/admin',(req,res)=>{
 
